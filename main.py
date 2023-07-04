d3f455dd3efb573e0677d233199fc9e8138de261
sglacy1 <sglacy1@asu.edu>
2023-07-04 15:01:14 -0700

    remove key

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from flask_gravatar import Gravatar
 from forms import RegisterForm, CreatePostForm, LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
