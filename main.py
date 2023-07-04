f66a46c995a7b08c18af596493530e2551d77ce4
sglacy1 <sglacy1@asu.edu>
2023-07-04 13:31:39 -0700

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from functools import wraps
+import os
 
 from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
@@ -18,12 +19,12 @@ from flask_gravatar import Gravatar
 from forms import RegisterForm, CreatePostForm, LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQoeLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
